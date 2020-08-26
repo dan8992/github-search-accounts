6d7c0da02070789799c0f3570ecda6a22778556b
System Administrator <root@Dans-MacBook-Pro.local>
2020-08-26 20:30:52 +0300

    Github search accounts

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About Github Accounts</h1>
+      <p>Application to search Github accounts</p>
+    </Fragment>
+  );
+};
+
+export default About;
